6d0638a094521292910ceebb7bfea08df88a6490
nilaxsuvisinth <nilaxsuvisinth27@gmail.com>
2024-01-30 22:31:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Music-Player
+
+A music webpage designed using HTML, CSS & JavaScript that can play songs, skip them and change them accordingly.
---
+++ js/music-list.js
@@ -0,0 +1,30 @@
+
+
+let allMusic = [
+    {
+      name: "Aval Ulaga Azhakiye",
+      artist: "Karthick",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Thee Thalapathy",
+      artist: "STR",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Vaarayo Vaarayo",
+      artist: "P. Unnikrishnan, Chinmayi, Megha",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Oru Malai",
+      artist: "Harris Jayaraj",
+      img: "music-4",
+      src: "music-4"
+    },
+
+  
+  ];
